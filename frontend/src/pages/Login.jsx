f81333aec8c1d2bd4ac5ab728790a1e21e52e608
Siddhii22 <nishanksiddhigupta@gmail.com>
2026-04-23 01:04:12 +0530

    Fixed API URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", form);
+      const res = await axios.post("https://mern-project-9u8s.onrender.com/api/auth/login", form)
 
       localStorage.setItem("token", res.data.token);
       window.location.href = "/dashboard";
